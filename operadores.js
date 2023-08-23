ef7909dbedbad4b68f2a7acbf0356de72e5c5642
nicolefernandads <137309766+nicolefernandads@users.noreply.github.com>
2023-08-23 12:06:37 -0300

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,31 @@
+function somar(){
+var val1 = document.getElementById('valor1').value;
+var val2 = document.getElementById('valor2').value;
+var result = parseInt(val1) + parseInt(val2);
+alert(result);
+
+}
+
+function sub(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) - parseInt(val2);
+    alert(result);
+
+}
+
+function divisão(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) / parseInt(val2);
+    alert(result);
+
+}
+
+function multi(){
+    var val1 = document.getElementById('valor1').value;
+    var val2 = document.getElementById('valor2').value;
+    var result = parseInt(val1) * parseInt(val2);
+    alert(result);
+
+}
